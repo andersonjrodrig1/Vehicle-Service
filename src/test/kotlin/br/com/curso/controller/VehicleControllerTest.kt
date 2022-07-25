2fbfcdab6baf35e157629bbc44b69ac5d0b1e16c
Anderson Junior Rodrigues <andersonkz@ymail.com>
2022-07-25 18:16:59 -0300

    Contrução de testes unitários

--- src/test/kotlin/br/com/curso/controller/VehicleControllerTest.kt
+++ src/test/kotlin/br/com/curso/controller/VehicleControllerTest.kt
@@ -15,8 +15,22 @@ class VehicleControllerTest: FunSpec({
 
     val vehicleControllerMock = mockk<VehicleController>()
     val vehicleMock = mockk<Vehicle>()
+    val listVehicle = listOf(
+        Vehicle(
+            id = 1,
+            model = "modelo test1",
+            brand = "marca test1",
+            plaque = "TTT 1234"
+        )
+    )
+    val vehicle = Vehicle(
+        id = 1,
+        model = "modelo test1",
+        brand = "marca test1",
+        plaque = "TTT 1234"
+    )
 
-    test("test getAll") {
+    test("test getAll controller") {
         every {
             vehicleControllerMock.getAll()
         } returns HttpResponse.ok(
@@ -33,19 +47,10 @@ class VehicleControllerTest: FunSpec({
         val result = vehicleControllerMock.getAll()
 
         result.status shouldBe HttpStatus.OK
-        result.body shouldBe Optional.of(
-            listOf<Vehicle>(
-                Vehicle(
-                    id = 1,
-                    model = "modelo test1",
-                    brand = "marca test1",
-                    plaque = "TTT 1234"
-                )
-            )
-        )
+        result.body shouldBe Optional.of(listVehicle)
     }
 
-    test("test post vehicle") {
+    test("test post controller") {
         every {
             vehicleControllerMock.create(vehicle = vehicleMock)
         } returns HttpResponse.created(
@@ -60,17 +65,10 @@ class VehicleControllerTest: FunSpec({
         val result = vehicleControllerMock.create(vehicle = vehicleMock)
 
         result.status shouldBe HttpStatus.CREATED
-        result.body shouldBe Optional.of(
-            Vehicle(
-                id = 1,
-                model = "modelo test1",
-                brand = "marca test1",
-                plaque = "TTT 1234"
-            )
-        )
+        result.body shouldBe Optional.of(vehicle)
     }
 
-    test("test findById vehicle") {
+    test("test findById controller") {
         every {
             vehicleControllerMock.findById(id = 1)
         } returns HttpResponse.ok(
@@ -88,18 +86,11 @@ class VehicleControllerTest: FunSpec({
 
         result.status shouldBe HttpStatus.OK
         result.body shouldBe Optional.of(
-            Optional.of(
-                Vehicle(
-                    id = 1,
-                    model = "modelo test1",
-                    brand = "marca test1",
-                    plaque = "TTT 1234"
-                )
-            )
+            Optional.of(vehicle)
         )
     }
 
-    test("test update vehicle") {
+    test("test update controller") {
         every {
             vehicleControllerMock.update(vehicle = vehicleMock)
         } returns HttpResponse.ok(
@@ -114,17 +105,10 @@ class VehicleControllerTest: FunSpec({
         val result = vehicleControllerMock.update(vehicle = vehicleMock)
 
         result.status shouldBe HttpStatus.OK
-        result.body shouldBe Optional.of(
-            Vehicle(
-                id = 1,
-                model = "modelo test1",
-                brand = "marca test1",
-                plaque = "TTT 1234"
-            )
-        )
+        result.body shouldBe Optional.of(vehicle)
     }
 
-    test("test delete vehicle") {
+    test("test delete controller") {
         every {
             vehicleControllerMock.delete(id = 1)
         } returns HttpResponse.ok()
--- src/test/kotlin/br/com/curso/service/VehicleServiceTest.kt
+++ src/test/kotlin/br/com/curso/service/VehicleServiceTest.kt
@@ -1,12 +1,106 @@
 package br.com.curso.service
-import io.micronaut.runtime.EmbeddedApplication
-import io.micronaut.test.extensions.junit5.annotation.MicronautTest
-import org.junit.jupiter.api.Assertions
-import org.junit.jupiter.api.Test
-import jakarta.inject.Inject
+
+import br.com.curso.model.Vehicle
+import io.kotest.core.spec.style.FunSpec
+import io.kotest.matchers.shouldBe
+import io.micronaut.test.extensions.kotest.annotation.MicronautTest
+import io.mockk.every
+import io.mockk.mockk
+import org.junit.jupiter.api.Assertions.assertTrue
+import java.util.*
 
 @MicronautTest
-class VehicleServiceTest {
+class VehicleServiceTest: FunSpec({
 
+    val vehicleServiceMock = mockk<VehicleService>()
+    val vehicleMock = mockk<Vehicle>()
+    val listVehicle = mutableListOf(
+        Vehicle(
+            id = 1,
+            model = "modelo test1",
+            brand = "marca test1",
+            plaque = "TTT 1234"
+        )
+    )
+    val vehicle = Vehicle(
+        id = 1,
+        model = "modelo test1",
+        brand = "marca test1",
+        plaque = "TTT 1234"
+    )
 
-}
+    test("test getAll service") {
+        every {
+            vehicleServiceMock.getAll()
+        } returns mutableListOf(
+            Vehicle(
+                id = 1,
+                model = "modelo test1",
+                brand = "marca test1",
+                plaque = "TTT 1234"
+            )
+        )
+
+        val result = vehicleServiceMock.getAll()
+
+        result shouldBe listVehicle
+    }
+
+    test("test create service") {
+        every {
+            vehicleServiceMock.create(vehicle = vehicleMock)
+        } returns Vehicle(
+            id = 1,
+            model = "modelo test1",
+            brand = "marca test1",
+            plaque = "TTT 1234"
+        )
+
+        val result = vehicleServiceMock.create(vehicle = vehicleMock)
+
+        result shouldBe vehicle
+    }
+
+    test("test findById service") {
+        every {
+            vehicleServiceMock.findById(id = 1)
+        } returns Optional.of(
+            Vehicle(
+                id = 1,
+                model = "modelo test1",
+                brand = "marca test1",
+                plaque = "TTT 1234"
+            )
+        )
+
+        val result = vehicleServiceMock.findById(id = 1)
+
+        result shouldBe Optional.of(vehicle)
+    }
+
+    test("test update service") {
+        every {
+            vehicleServiceMock.update(vehicle = vehicleMock)
+        } returns Vehicle(
+            id = 1,
+            model = "modelo test1",
+            brand = "marca test1",
+            plaque = "TTT 1234"
+        )
+
+        val result = vehicleServiceMock.update(vehicle = vehicleMock)
+
+        result shouldBe vehicle
+    }
+
+    test("test delete service") {
+        every {
+            vehicleServiceMock.delete(id = 1)
+        } returns Unit
+
+        vehicleServiceMock.delete(id = 1)
+
+        assertTrue(true)
+    }
+
+})
