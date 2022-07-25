dcdff07989558da902503d3ac1f8adf08da0019a
Anderson Junior Rodrigues <andersonkz@ymail.com>
2022-07-24 22:45:13 -0300

    Contrução de testes unitários

--- src/test/kotlin/br/com/curso/controller/VehicleControllerTest.kt
+++ src/test/kotlin/br/com/curso/controller/VehicleControllerTest.kt
@@ -2,7 +2,6 @@ package br.com.curso.controller
 
 import br.com.curso.model.Vehicle
 import io.kotest.core.spec.style.FunSpec
-import io.kotest.matchers.reflection.beLateInit
 import io.kotest.matchers.shouldBe
 import io.micronaut.http.HttpResponse
 import io.micronaut.http.HttpStatus
@@ -58,7 +57,7 @@ class VehicleControllerTest: FunSpec({
             )
         )
 
-        val result = vehicleControllerMock.create(vehicleMock)
+        val result = vehicleControllerMock.create(vehicle = vehicleMock)
 
         result.status shouldBe HttpStatus.CREATED
         result.body shouldBe Optional.of(
@@ -88,14 +87,51 @@ class VehicleControllerTest: FunSpec({
         val result = vehicleControllerMock.findById(id = 1)
 
         result.status shouldBe HttpStatus.OK
-        result.body shouldBe Optional.of(Optional.of(
+        result.body shouldBe Optional.of(
+            Optional.of(
+                Vehicle(
+                    id = 1,
+                    model = "modelo test1",
+                    brand = "marca test1",
+                    plaque = "TTT 1234"
+                )
+            )
+        )
+    }
+
+    test("test update vehicle") {
+        every {
+            vehicleControllerMock.update(vehicle = vehicleMock)
+        } returns HttpResponse.ok(
             Vehicle(
                 id = 1,
                 model = "modelo test1",
                 brand = "marca test1",
                 plaque = "TTT 1234"
             )
-        ))
+        )
+
+        val result = vehicleControllerMock.update(vehicle = vehicleMock)
+
+        result.status shouldBe HttpStatus.OK
+        result.body shouldBe Optional.of(
+            Vehicle(
+                id = 1,
+                model = "modelo test1",
+                brand = "marca test1",
+                plaque = "TTT 1234"
+            )
+        )
+    }
+
+    test("test delete vehicle") {
+        every {
+            vehicleControllerMock.delete(id = 1)
+        } returns HttpResponse.ok()
+
+        val result = vehicleControllerMock.delete(id = 1)
+
+        result.status shouldBe HttpStatus.OK
     }
 
 })
